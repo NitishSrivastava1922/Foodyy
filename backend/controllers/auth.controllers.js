8057d7c5c54e85d4e42f1e45829db9693844e7db
NitishSrivastava1922 <mannsrivastava49@gmail.com>
2026-07-07 19:40:56 +0530

    Update auth.controllers.js

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -70,12 +70,14 @@ export const signIn=async (req,res) => {
 }
 
 export const signOut=async (req,res) => {
+   export const signOut=async (req,res) => {
     try {
         res.clearCookie("token")
 return res.status(200).json({message:"log out successfully"})
     } catch (error) {
         return res.status(500).json(`sign out error ${error}`)
     }
+}
 }
 
 export const sendOtp=async (req,res) => {
